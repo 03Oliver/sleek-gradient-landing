ecba175b6c3c0ea15f6074528692f842c3f56c92
03Oliver <109152422+03Oliver@users.noreply.github.com>
2024-05-28 13:40:06 +0100

    overflowfix

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -29,7 +29,7 @@ const Index = () => {
   return (
     <Container centerContent maxW="600vw" height="100vh" display="flex" flexDirection="column" justifyContent="center" alignItems="center" bgGradient="linear(to-r, black, gray.800)" color="white" fontFamily="Roboto, sans-serif">
       <VStack spacing={8} textAlign="center">
-        <Box as="pre" fontSize="2xl" whiteSpace="pre-wrap" overflow="hidden" borderRight="2px solid" animation={`${typing} 4s steps(${fullText.length}), ${blink} 0.75s step-end infinite`}>
+        <Box as="pre" fontSize="2xl" whiteSpace="pre-wrap" borderRight="2px solid" animation={`${typing} 4s steps(${fullText.length}), ${blink} 0.75s step-end infinite`}>
           {text}
         </Box>
         <Link href="https://www.linkedin.com/company/collectivevc" isExternal>
